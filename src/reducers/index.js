41f648c13e9079fb33da70948ab1696ab68edd87
Alex Hernandez <alexh385@gmail.com>
2018-03-18 16:53:38 -0400

    Import and implement:
    form reducer from redux-form
    
    brews reducer
    
    brew detail reducer
    
    modal reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,15 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
+import BrewsReducer from './brews_reducer';
+import BrewDetailReducer from './brew_detail_reducer';
+import ModalReducer from './modal_reducer';
+
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  brews: BrewsReducer,
+  form: formReducer,
+  brew: BrewDetailReducer,
+  showModal: ModalReducer
 });
-
+debugger;
 export default rootReducer;
