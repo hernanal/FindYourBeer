02fed2290c49fc1a57e2a9f784ec38cbf8ce2738
Alex Hernandez <alexh385@gmail.com>
2018-03-18 16:24:43 -0400

    Import and implement BrowserRouter, Route, and Switch from react-router.
    Import redux-promise
    
    Import redux-thunk
    
    Import the landing page, brew_list, and add_brew components

--- src/index.js
+++ src/index.js
@@ -2,14 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Promise from 'redux-promise';
+import Thunk from 'redux-thunk';
+
 
 import App from './components/app';
 import reducers from './reducers';
+import LandingPage from './components/landing';
+import BrewList from './containers/brew_list';
+import AddBrew from './components/add_brew';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const createStoreWithMiddleware = applyMiddleware(Promise, Thunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <Route path="/brews/add" component={AddBrew} />
+          <Route path="/brews" component={BrewList} />
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
